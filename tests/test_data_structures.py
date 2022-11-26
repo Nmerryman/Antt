ca616cf6147db7b5011b49664d945ba52941cb67
Nmerryman <nmerryman9001@gmail.com>
2022-11-26 00:18:12 -0600

    Made a first runnable test

---
+++ tests/test_data_structures.py
@@ -0,0 +1,24 @@
+import pytest
+
+import antt.data_structures as ds
+from time import sleep
+
+assert_timeout = 1
+assert_delay = .05
+
+
+@pytest.mark.filterwarnings("error")
+def test_basic_socket_life():
+    conn = ds.SocketConnection(ds.get_first_port_from(10000), ("", 2222))
+    conn.start()
+    conn.in_queue.put("kill")
+
+    count = 0
+    while conn.is_alive() and count < int(assert_timeout / assert_delay):
+        sleep(assert_delay)
+        count += 1
+
+    assert not conn.is_alive()
+    assert not conn.alive
+
+
