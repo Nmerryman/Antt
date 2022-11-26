b3a83ea61c2ac910773a0697d6b77ad6d97f93de
Nmerryman <nmerryman9001@gmail.com>
2022-11-26 00:18:13 -0600

    Added a test to have one conn send a message to the other one

--- tests/test_data_structures.py
+++ tests/test_data_structures.py
@@ -3,8 +3,9 @@ import pytest
 import antt.data_structures as ds
 from time import sleep
 
-assert_timeout = 1
+assert_timeout = 3
 assert_delay = .01
+assert_limit = int(assert_timeout / assert_delay)
 # pytest.mark.filterwarnings("error")
 
 
@@ -19,11 +20,46 @@ def test_basic_socket_life():
     conn.in_queue.put("kill")
 
     count = 0
-    while conn.is_alive() and count < int(assert_timeout / assert_delay):
+    while conn.is_alive() and count < assert_limit:
         sleep(assert_delay)
         count += 1
 
     assert not conn.is_alive()
     assert not conn.alive
+
+
+@pytest.mark.filterwarnings
+def test_basic_socket_send():
+    """
+    Send a basic message from one comm to the other
+    """
+    port_sender = ds.get_first_port_from(2222)
+    port_receiver = ds.get_first_port_from(3333)
+    sender = ds.SocketConnection(port_sender, ("127.0.0.1", port_receiver))
+    receiver = ds.SocketConnection(port_receiver, ("127.0.0.1", port_sender))
+
+    sender.start()
+    receiver.start()
+
+    # Wait for both to be alive
+    count = 0
+    while not receiver.alive and count < assert_limit:
+        sleep(assert_delay)
+        count += 1
+    assert count != assert_limit
+
+    # Wait for message to be sent
+    sender.in_queue.put(b"Hello")
+    count = 0
+    while not sender.in_queue.empty() and count < assert_limit:
+        sleep(assert_delay)
+        count += 1
+    assert count != assert_limit
+
+    message = receiver.block_until_message()
+    sender.in_queue.put("kill")
+    receiver.in_queue.put("kill")
+
+    assert message == b"Hello"
 
 
