75898bd2a67c5b0e456ad2649dd79526dc4de5bb
Nmerryman <nmerryman9001@gmail.com>
2022-11-28 22:29:58 -0600

    Updated for current tests

--- tests/debug_basic_udp_sother.py
+++ tests/debug_basic_udp_sother.py
@@ -1,15 +1,17 @@
-# Client A
+# Client B
 import socket
 import antt.data_structures as ds
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(("", ds.get_first_port_from(20000)))
-s.settimeout(.1)
+s.bind(("", ds.get_first_port_from(25000)))
+s.settimeout(0.1)
 
-# packet = ds.Packet("status", "aoeu")
-packet = ds.Packet("auth", "aoeu", "info")
+packet = ds.Packet("status", "aoeu")
+# packet = ds.Packet("auth", "aoeu", "info2")
 s.sendto(packet.generate(), ("127.0.0.1", 12345))
 print(s.recv(200))
+
+
 
 
 
