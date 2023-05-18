01c55356535511bd1284ee915ed201dbe85cd4e8
Nmerryman <nmerryman9001@gmail.com>
2023-05-18 02:20:42 -0500

    Added a server implimentation

---
+++ examples/shell_client.py
@@ -0,0 +1,31 @@
+import antt.data_structures as ds
+
+if __name__ == '__main__':
+    lport = 33553
+    dport = 33773
+    client = ds.SocketConnectionUDP(lport, ("127.0.0.1", dport))
+
+    client.start()
+    client.block_until_verify()
+
+    print("""Imagine a shell
+cd [dir name] - as expected
+ls - as expected
+dl [src name] - Download file""")
+
+    while client.alive and client.verified_connection:
+        command = input("> ")
+        client.in_queue.put(ds.Packet(*command.split()).generate())
+
+        response_raw = client.block_until_message()
+        response = ds.Packet().parse(response_raw)
+
+        if response.type == "ls":
+            print("Dirs:", response.value)
+            print("Files:", response.data)
+        elif response.type == "cd":
+            print(response.value)
+        elif response.type == "file":
+            local_name = input("Save as name> ")
+            with open(local_name, "wb") as f:
+                f.write(response.value)
---
+++ examples/shell_server.py
@@ -0,0 +1,40 @@
+import antt.data_structures as ds
+import os
+import time
+
+if __name__ == '__main__':
+    lport = 33773
+    dport = 33553
+    server = ds.SocketConnectionUDP(lport, ("127.0.0.1", dport))
+
+    server.start()
+    server.block_until_verify()
+
+    while server.alive and server.verified_connection:
+        if not server.out_queue.empty():
+            message_raw = server.block_until_message()  # We could manually take it out of the queue an task_done() it
+            message = ds.Packet().parse(message_raw)
+
+            if message.type == "ls":
+                dir_obs = os.listdir()
+                dirs = []
+                files = []
+                for a in dir_obs:
+                    if os.path.isfile(a):
+                        files.append(a)
+                    else:
+                        dirs.append(a)
+                server.in_queue.put(ds.Packet("ls", dirs, files).generate())
+            elif message.type == "cd":
+                prev = os.path.basename(os.getcwd())
+                os.chdir(message.value)
+                server.in_queue.put(ds.Packet("cd", f"{prev}->{message.value}").generate())
+            elif message.type == "dl":
+                with open(message.value, 'rb') as f:
+                    data = f.read()
+                    server.in_queue.put(ds.Packet("data", data).generate())
+
+        else:
+            # So we don't just spin away at max speed
+            time.sleep(.1)
+
